0285a9dd023cdb5c7e4377ab17577baf127e10f6
zoadev <zoa24434@gmail.com>
2025-04-08 16:22:48 +0700

    update ExportOptions.plist

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page +3'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page +4'),
     );
   }
 }
